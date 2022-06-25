c12eb4d901c5a8ea798072b4d6f8b020f6969032
someee <79901950+sommizzu@users.noreply.github.com>
2022-06-25 19:18:43 +0000

    파이썬 리스트와 튜플의 차이점 및 개념

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-#string = 'abcdfe2a354a32a' -> 맨 앞글자 대문자로 바꾸기
-
-string = 'abcdfe2a354a32a'
-string = string.replace('a', 'A')
-print(string)
+#파이선 리스트는 순서가 있고 수정 가능한 자료구조이다.
+#파이썬 튜플은 순서가 있지만 수정 불가능한 자료구조이다.
