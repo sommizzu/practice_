3592a7176d15cdfa135f3890ad1e11bdba544a5a
someee <79901950+sommizzu@users.noreply.github.com>
2022-06-28 13:51:34 +0000

    temp라는 이름을 가진 비어있는 딕셔너리 만들기

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-#10개의 값이 저장된 scores 리스트가 있을 때, start expression을 사용하여 가운데 있는 8개의 값을 valid_score 변수에 바인딩하여라.
+#비어있는 딕셔너리
 
-scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
-
-scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
-a, *valid_score, b = scores
-
-print(valid_score)
-
+temp = { }
