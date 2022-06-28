bb29db003784beb2620b6ff97449e2ada27989d4
someee <79901950+sommizzu@users.noreply.github.com>
2022-06-28 14:00:50 +0000

    icecream 딕셔너리에서 아이스크림 판매 금액의 총합출력

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-#딕셔너리 value값으로만 출력된 리스트 출력하기
+#icecream 딕셔너리에서 아이스크림 판매 금액의 총합을 출력
 icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
 
 icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
-price = list(icecream.values())
-print(price)
+print(sum(icecream.values()))
