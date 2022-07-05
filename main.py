b5b3bf79de3453eea587e6c63cb646832ce0611f
someee <79901950+sommizzu@users.noreply.github.com>
2022-07-05 00:51:15 +0000

    상속 클래스

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# 차 클래스를 상속받은 자전차 클래스를 정의하세요.
+
 class 차:
     def __init__(self, 바퀴, 가격):
         self.바퀴 = 바퀴
         self.가격 = 가격
 
 
-car = 차(2, 1000)
-print(car.바퀴)
-print(car.가격)
+class 자전차(차):
+    pass
