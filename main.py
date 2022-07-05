8c6259e960d4a4fba5f46366649cace818215c34
someee <79901950+sommizzu@users.noreply.github.com>
2022-07-05 00:52:19 +0000

    자전차 클래스는 차 클래스를 상속, 자전차 클래스 정의

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# 차 클래스를 상속받은 자전차 클래스를 정의하세요.
-
 class 차:
     def __init__(self, 바퀴, 가격):
         self.바퀴 = 바퀴
@@ -7,4 +5,11 @@ class 차:
 
 
 class 자전차(차):
-    pass
+    def __init__(self, 바퀴, 가격):
+        self.바퀴 = 바퀴
+        self.가격 = 가격
+
+
+bicycle = 자전차(2, 100)
+print(bicycle.가격)
+
