48d397a2345ad8f481667e84fa7e56f4fe50557c
someee <79901950+sommizzu@users.noreply.github.com>
2022-07-05 00:50:52 +0000

    클래스 정의

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#사람 (Human) 클래스의 인스턴스를 "생성" 하고 이를 areum 변수로 바인딩
+class 차:
+    def __init__(self, 바퀴, 가격):
+        self.바퀴 = 바퀴
+        self.가격 = 가격
 
-class Human:
-    pass
 
-areum = Human()
-
-print(Human)
+car = 차(2, 1000)
+print(car.바퀴)
+print(car.가격)
