e35c827721384c59789a03ff14afe91cb874c2c4
someee <79901950+sommizzu@users.noreply.github.com>
2022-07-05 00:54:04 +0000

    자전차 정보로 구동계 정보까지 출력하도록 수정 _부모 클래스 메서드 호출

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ class 자전차(차):
         super().__init__(바퀴, 가격)
         self.구동계 = 구동계
 
+    def 정보(self):
+        super().정보()
+        print("구동계 ", self.구동계)
+
 bicycle = 자전차(2, 100, "시마노")
 bicycle.정보()
+
 
