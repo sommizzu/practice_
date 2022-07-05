798abccc7d960dc68e76ae5ba6f81778f43ff932
someee <79901950+sommizzu@users.noreply.github.com>
2022-07-05 00:53:16 +0000

    부모 클래스 생성자 호출

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ class 차:
         self.바퀴 = 바퀴
         self.가격 = 가격
 
+    def 정보(self):
+        print("바퀴수 ", self.바퀴)
+        print("가격 ", self.가격)
+
+class 자동차(차):
+    def __init__(self, 바퀴, 가격):
+        super().__init__(바퀴, 가격)
 
 class 자전차(차):
-    def __init__(self, 바퀴, 가격):
-        self.바퀴 = 바퀴
-        self.가격 = 가격
+    def __init__(self, 바퀴, 가격, 구동계):
+        super().__init__(바퀴, 가격)
+        self.구동계 = 구동계
 
-
-bicycle = 자전차(2, 100)
-print(bicycle.가격)
+bicycle = 자전차(2, 100, "시마노")
+bicycle.정보()
 
