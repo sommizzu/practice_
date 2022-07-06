55d5ad18eec16fd5ac0659182d345947f71d7afe
someee <79901950+sommizzu@users.noreply.github.com>
2022-07-06 12:06:48 +0000

    객체에 종목코드를 입력할 수 있는 set_code 메서드를 추가

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-class 차:
-    def __init__(self, 바퀴, 가격):
-        self.바퀴 = 바퀴
-        self.가격 = 가격
+#객체에 종목코드를 입력할 수 있는 set_code 메서드를 추가
+class Stock:
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
 
-    def 정보(self):
-        print("바퀴수 ", self.바퀴)
-        print("가격 ", self.가격)
+    def set_name(self, name):
+        self.name = name
 
-class 자동차(차):
-    def __init__(self, 바퀴, 가격):
-        super().__init__(바퀴, 가격)
+    def set_code(self, code):
+        self.code = code
 
-class 자전차(차):
-    def __init__(self, 바퀴, 가격, 구동계):
-        super().__init__(바퀴, 가격)
-        self.구동계 = 구동계
-
-    def 정보(self):
-        super().정보()
-        print("구동계 ", self.구동계)
-
-bicycle = 자전차(2, 100, "시마노")
-bicycle.정보()
-
+a = Stock(None, None)
+a.set_code("005930")
+print(a.code)
 
