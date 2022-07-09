46e632e0f716c1cc9b22eec0c02ebc34ee79f335
someee <79901950+sommizzu@users.noreply.github.com>
2022-07-09 08:33:53 +0000

    파이썬 클래스

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-#객체에 종목코드를 입력할 수 있는 set_code 메서드를 추가
-class Stock:
-    def __init__(self, name, code):
-        self.name = name
-        self.code = code
-
-    def set_name(self, name):
-        self.name = name
-
-    def set_code(self, code):
-        self.code = code
-
-a = Stock(None, None)
-a.set_code("005930")
-print(a.code)
-
+>>> class Cookie:
+>>>    pass
