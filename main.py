1925f4692da731be01c90ef8cb743efaa721715d
someee <79901950+sommizzu@users.noreply.github.com>
2022-07-16 15:42:43 +0000

    test

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-import random
+print('A1','A2',sep=',')
 
-class Account:
-    # class variable
-    account_count = 0
-
-    def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.bank = "SC은행"
-
-        num1 = random.randint(0, 999)
-        num2 = random.randint(0, 99)
-        num3 = random.randint(0, 999999)
-
-        num1 = str(num1).zfill(3)      # 1 -> '1' -> '001'
-        num2 = str(num2).zfill(2)      # 1 -> '1' -> '01'
-        num3 = str(num3).zfill(6)      # 1 -> '1' -> '0000001'
-        self.account_number = num1 + '-' + num2 + '-' + num3  # 001-01-000001
-
-        Account.account_count += 1
-
-
-kim = Account("정소미1", 100)
-print(Account.account_count)
-lee = Account("정소미", 100)
-print(Account.account_count)
-
-### test
+#Sep 파라미터로 구분자 지정
